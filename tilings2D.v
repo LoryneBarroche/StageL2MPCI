6079dcdf48fa696b87ecda41883887d7c7d5bc34
felixcastroi2m <felix.castro@univ-amu.fr>
2026-06-22 16:57:24 +0200

    Update tilings2D.v
    updated up to  Definition Compatible (etc.)

--- tilings2D.v
+++ tilings2D.v
@@ -1,6 +1,7 @@
 Require Import Bool.
 Require Export ZArith.
 Require Import Psatz.
+Require Import List.
 From mathcomp Require Import ssreflect ssrbool ssrfun eqtype ssrnat.
 From mathcomp Require Import seq. (* path fintype. *)
 (* From mathcomp Require Import fingraph. *)
@@ -10,7 +11,14 @@ From mathcomp Require Import seq. (* path fintype. *)
 
 (** We define colors as natural numbers, and we set a few
     names to make things a bit more readable. *)
+
+Definition type_Dec (X: Type) := forall (x y : X), {x = y}+{x <> y}.
+
+Inductive colorX {X : Type}{p : type_Dec X} := ColorX: X -> colorX.
+
+
 Inductive color := Color: nat -> color.
+
 
 (** The equality of colors is of course decidable. *)
 
@@ -18,6 +26,19 @@ Definition color_eqb : color -> color -> bool.
 Proof.
   intros t u. destruct t, u.
   exact (Nat.eqb n n0).
+Defined.
+
+
+Inductive color1D := Color1D: (list nat) -> color1D.
+
+
+(** The equality of colors is of course decidable. *)
+
+Definition color1D_eqb : color1D -> color1D -> bool.
+Proof.
+  intros t u. destruct t, u.
+  destruct (list_eq_dec Nat.eq_dec l l0).
+  exact true. exact false.
 Defined.
 
 Definition Black := Color 0.
@@ -69,45 +90,67 @@ fun tile1 tile2 => color_eqb (@east tile1) (@west tile2).
 Definition compatible_south : tile -> tile -> bool:=
 fun tile1 tile2 => color_eqb (@south tile1) (@north tile2).
 
-
+Print Z.eqb.
 
 Infix "=z=":=Z.eqb (at level 75).
 
+Print option.
 
 Definition neighbour: cell -> cell -> option side:=
  fun c d =>
     match c with
-    | C xc yc =>
+    | C x1 y1 =>
         match d with
-        | C xd yd =>
-            if  (xd =z= Z.pred xc) && (yd =z= yc)
+        | C x2 y2 =>
+            if  (x2 =z= Z.pred x1) && (y2 =z= y1)
               then Some West
             else
-              if (xd =z= Z.succ xc) && (yd =z= yc)
+              if (x2 =z= Z.succ x1) && (y2 =z= y1)
                 then Some East
             else
-              if (xd =z= xc) && (yd =z= Z.pred yc)
+              if (x2 =z= x1) && (y2 =z= Z.pred y1)
                 then Some South
             else
-              if (xd =z= xc) && (yd =z= Z.succ yc)
+              if (x2 =z= x1) && (y2 =z= Z.succ y1)
                 then Some North
             else
               None
         end 
     end.
-
+ 
 Inductive neighbour_spec: cell -> cell-> option side -> Type:=
 |Neighbour_west (x1 y1 x2 y2:Z) (p:x2 = Z.pred x1) (q:y2 = y1): neighbour_spec (C x1 y1) (C x2  y2) (Some West)
 |Neighbour_east x1 y1 x2 y2     (p:x2 = Z.succ x1) (q:y2 = y1): neighbour_spec (C x1 y1) (C x2 y2) (Some East)
 |Neighbour_south x1 y1 x2 y2 (p:x2 = x1) (q:y2 = Z.pred y1): neighbour_spec (C x1 y1) (C x2 y2) (Some South)
 |Neighbour_north x1 y1 x2 y2 (p:x2 = x1) (q:y2 = Z.succ y1): neighbour_spec (C x1 y1) (C x2 y2) (Some North)
-|Neighbour_none x1 y1 x2 y2 (p:x2<>Z.pred x1) (q:x2<>Z.succ x1) (r:y2<>Z.pred y1) (s:y2<>Z.succ y1): neighbour_spec (C x1 y1) (C x2 y2) None.
+|Neighbour_none x1 y1 x2 y2
+(p : not ((x2 = Z.pred x1) /\ (y2 = y1)))
+(q : not((x2 = Z.succ x1) /\ (y2 = y1)))
+(r : not((x2 = x1) /\ (y2 = Z.pred y1)))
+(s : not((x2 = x1) /\ (y2 = Z.succ y1))) : neighbour_spec (C x1 y1) (C x2 y2) None.
 
+
+Lemma Zeq_is_neq_zeq_false : forall x y, x <> y -> (x =z= y) = false. 
+Proof. 
+  intros. assert (x = y <->  (x =z= y) = true).
+  exact (Zeq_is_eq_bool x y). destruct (x =z= y). 
+  - destruct H0. edestruct H. apply H1. auto. 
+  - auto.
+Qed.
+
+Lemma Zeq_is_eq_zeq_true : forall x y, x = y -> (x =z= y) = true. 
+Proof. 
+  intros.
+  assert (x = y <->  (x =z= y) = true).
+  exact (Zeq_is_eq_bool x y). destruct (x =z= y). 
+  - auto. 
+  -  destruct H0. apply H0. auto.
+Qed.
 
 Lemma neighbourP:
   forall c1 c2, neighbour_spec c1 c2 (neighbour c1 c2).
 Proof.
-  intros. destruct c1, c2.
+  intros. destruct c1 as [x1 y1], c2 as [x2 y2].
   unfold neighbour.
   - case:ifP. intro. 
   apply andb_prop in i. destruct i. apply Z.eqb_eq in H, H0.
@@ -121,13 +164,25 @@ Proof.
   - case:ifP. intros. 
   apply andb_prop in i. destruct i. apply Z.eqb_eq in H, H0.
   apply Neighbour_north. apply H. apply H0. 
-  - intros. apply Neighbour_none.
-  pose (b:= (z1 =z= Z.pred z)).
-  assert ((z1 =z= Z.pred z) = b) by reflexivity.
-  destruct b. apply Z.eqb_eq in H. destruct H. apply Z.eqb_neq. unfold Z.eqb. Admitted. 
-  
-   (*Search (_ && _).*)
-  Print(&&).
-  
+  - intros. constructor.
+    + intro. destruct H. rewrite H in n2. rewrite H0 in n2. 
+      rewrite (Zeq_is_eq_zeq_true (Z.pred x1) (Z.pred x1)) in n2.
+      rewrite (Zeq_is_eq_zeq_true (y1) (y1)) in n2.
+      simpl in n2. discriminate. auto. auto.
+    + intro. destruct H. 
+    rewrite (Zeq_is_eq_zeq_true (x2) (Z.succ x1)) in n1.
+    rewrite (Zeq_is_eq_zeq_true (y2) (y1)) in n1.
+    discriminate. 
+    auto. auto. 
++ Admitted.
 
 
+Definition compatible: (P :configuration) (C1 : cell) (C2 : Cell) : bool:=
+match neighbour C1 C2 with
+| Some North => compatible_right (T c) (T d)
+| Some South =>
+| Some East =>
+| Some West =>
+| None => true
+end. 
+ 
